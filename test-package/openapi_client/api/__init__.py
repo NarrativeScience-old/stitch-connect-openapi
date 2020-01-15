8222f1d3885e985b5fa8b10347ebaf329879a2a8
Annie Xu <axu@narrativescience.com>
2020-01-15 11:07:32 -0600

    move package out

--- test-package/openapi_client/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.destination_types_api import DestinationTypesApi
-from openapi_client.api.destinations_api import DestinationsApi
-from openapi_client.api.sources_api import SourcesApi
